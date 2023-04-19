c3fd0eb22bc3d47bccfd9480e3ac81643925c53c
VithoriaS <vivilcsantos@gmail.com>
2023-04-19 07:50:40 -0300

    Comentarios Hash

--- TP1/HashExtensivel.java
+++ TP1/HashExtensivel.java
@@ -8,20 +8,17 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
-import org.xml.sax.SAXException;
-
 public class HashExtensivel {
    
-    String           nomeArquivoDiretorio;
-    String           nomeArquivoCestos;
+    String           nomeArqDiretorio;
+    String           nomeArqCestos;
     RandomAccessFile arqDiretório;
     RandomAccessFile arqCestos;
-    int              quantidadeDadosPorCesto;
+    int              qtdCesto;
     Diretório        diretório;
     Crud c;
     
     class Cesto {
-
         byte   profundidadeLocal;   // profundidade local do cesto
         short  quantidade;          // quantidade de pares presentes no cesto
         short  quantidadeMaxima;    // quantidade máxima de pares que o cesto pode conter
@@ -29,14 +26,15 @@ public class HashExtensivel {
         long[] dados;               // sequência de dados correspondentes às chaves
         short  bytesPorPar;         // size fixo de cada par de chave/dado em bytes
         short  bytesPorCesto;       // size fixo do cesto em bytes
-        Crud c;
+
 
         public Cesto(int qtdmax) throws Exception {
             this(qtdmax, 0);
         }
 
+
         public Cesto(int qtdmax, int pl) throws Exception {
-            if(qtdmax>32767)
+            if(qtdmax>8900)
                 throw new Exception("Quantidade máxima de 32.767 elementos");
             if(pl>127)
                 throw new Exception("Profundidade local máxima de 127 bits");
@@ -49,10 +47,16 @@ public class HashExtensivel {
             bytesPorCesto = (short)(bytesPorPar * quantidadeMaxima + 3);
         }
 
+        /**
+         * Transforma a estrutura de dados em um array de bytes.
+         * @return o array de bytes que representa a estrutura de dados.
+         * @throws IOException se ocorrer um erro de I/O durante a conversão.
+         */
         public byte[] toByteArray() throws IOException {
 
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             DataOutputStream dos = new DataOutputStream(baos);
+
             dos.writeByte(profundidadeLocal);
             dos.writeShort(quantidade);
             int i=0;
@@ -69,6 +73,13 @@ public class HashExtensivel {
             return baos.toByteArray();    
         }
 
+        
+        /**
+         * Carrega os dados da estrutura de dados a partir de um array de bytes.
+         * @param ba o array de bytes que representa a estrutura de dados.
+         * @throws IOException se ocorrer um erro de I/O durante a conversão.
+         */
+        
         public void fromByteArray(byte[] ba) throws IOException {
             ByteArrayInputStream bais = new ByteArrayInputStream(ba);
             DataInputStream dis = new DataInputStream(bais);
@@ -84,8 +95,12 @@ public class HashExtensivel {
              
         }
 
-        
-
+        /**
+         * Insere um novo par chave-valor ordenado na estrutura de dados.
+         * @param c A chave do novo elemento.
+         * @param d O endereco associado do novo elemento.
+         * @return true se a inserção foi bem-sucedida, false caso contrário.
+        */
         public boolean create(int c, long d) {
             if(full())
                 return false;
@@ -102,6 +117,11 @@ public class HashExtensivel {
             return true;
         }
 
+        /**
+         * Retorna o valor associado a uma chave na estrutura de dados.
+         * @param c A chave do elemento a ser buscado.
+         * @return O valor associado à chave buscada, ou -1 se a chave não foi encontrada.
+        */
         public long read(int c) {
             if(empty())
                 return -1;
@@ -114,6 +134,12 @@ public class HashExtensivel {
                 return -1;        
         }
 
+        /**
+         * Atualiza o valor associado a uma chave na estrutura de dados.
+         * @param c A chave do elemento a ser atualizado.
+         * @param d O novo endereço associado à chave do elemento.
+         * @return true se a atualização foi bem-sucedida, false caso contrário.
+        */
         public boolean update(int c, long d) {
             if(empty())
                 return false;
@@ -129,6 +155,11 @@ public class HashExtensivel {
                 return false;        
         }
 
+        /**
+         * Remove um elemento da estrutura de dados.
+         * @param c A chave do elemento a ser removido.
+         * @return true se a remoção foi bem-sucedida, false caso contrário.
+        */
         public boolean delete(int c) {
             if(empty())
                 return false;
@@ -148,10 +179,18 @@ public class HashExtensivel {
                 return false;        
         }
 
+        /**
+         * Verifica se a estrutura de dados está vazia.
+         * @return true se a estrutura está vazia, false caso contrário.
+         */
+
         public boolean empty() {
             return quantidade == 0;
         }
-
+        /**
+         * Verifica se a estrutura de dados está cheia.
+         * @return true se a estrutura está cheia, false caso contrário.
+         */
         public boolean full() {
             return quantidade == quantidadeMaxima;
         }
@@ -191,6 +230,9 @@ public class HashExtensivel {
         }
 
 
+        /**
+         * Função para recalcular a profundidade
+         */
         public boolean atualizaEndereco(int p, long e) {
             if(p>Math.pow(2,profundidadeGlobal))
                 return false;
@@ -198,6 +240,11 @@ public class HashExtensivel {
             return true;
         }
 
+        /**
+         * Transforma a estrutura de dados em um array de bytes.
+         * @return o array de bytes que representa a estrutura de dados.
+         * @throws IOException se ocorrer um erro de I/O durante a conversão.
+         */
         public byte[] toByteArray() throws IOException {
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             DataOutputStream dos = new DataOutputStream(baos);
@@ -212,6 +259,11 @@ public class HashExtensivel {
             return baos.toByteArray();            
         }
 
+        /**
+         * Carrega os dados da estrutura de dados a partir de um array de bytes.
+         * @param ba o array de bytes que representa a estrutura de dados.
+         * @throws IOException se ocorrer um erro de I/O durante a conversão.
+         */
         public void fromByteArray(byte[] ba) throws IOException {
             ByteArrayInputStream bais = new ByteArrayInputStream(ba);
             DataInputStream dis = new DataInputStream(bais);
@@ -242,6 +294,12 @@ public class HashExtensivel {
             return endereços[p];
         }
 
+        /**
+        *Duplica a tabela de endereços, aumentando a profundidade global em 1 e atualizando
+        *os novos endereços. Retorna true se a duplicação foi bem sucedida, false caso contrário.
+        *Se a profundidade global já for 127, não é possível realizar a duplicação.
+        *@return true se a duplicação foi bem sucedida, false caso contrário.
+        */
         protected boolean duplica() {
             if(profundidadeGlobal==127)
                 return false;
@@ -262,10 +320,21 @@ public class HashExtensivel {
             return true;
         }
 
+        /**
+         * Calcula o valor de hash de uma chave para a tabela de hash principal, usando a profundidade global.
+         * @param chave A chave para a qual o valor de hash será calculado.
+         * @return O valor de hash para a chave.
+        */
         protected int hash(int chave) {
             return chave % (int)Math.pow(2, profundidadeGlobal);
         }
 
+        /**
+         * Calcula o valor de hash de uma chave para uma tabela de hash específica, usando a profundidade local.
+         * @param chave A chave para a qual o valor de hash será calculado.
+         * @param pl A profundidade local da tabela de hash.
+         * @return O valor de hash para a chave.
+        */
         protected int hash2(int chave, int pl) { // cálculo do hash para uma dada profundidade local
             return chave % (int)Math.pow(2, pl);            
         }
@@ -273,16 +342,19 @@ public class HashExtensivel {
     }
     
     
-    
-    public HashExtensivel(int n, String nd, String nc ) throws Exception {
-        quantidadeDadosPorCesto = n;
-        nomeArquivoDiretorio = nd;
-        nomeArquivoCestos = nc;
+    /**
+     * @param Arq1 é o nome do arquivo que armazena o diretório.
+     * @param Arq2 é o nome do arquivo que armazena os cestos.
+    */
+    public HashExtensivel(int n, String Arq1, String Arq2 ) throws Exception {
+        qtdCesto = n;
+        nomeArqDiretorio = Arq1;
+        nomeArqCestos = Arq2;
         c = new Crud();
         
         
-        arqDiretório = new RandomAccessFile(nomeArquivoDiretorio,"rw");
-        arqCestos = new RandomAccessFile(nomeArquivoCestos,"rw");
+        arqDiretório = new RandomAccessFile(nomeArqDiretorio,"rw");
+        arqCestos = new RandomAccessFile(nomeArqCestos,"rw");
 
         // Se o diretório ou os cestos estiverem vazios, cria um novo diretório e lista de cestos
         if(arqDiretório.length()==0 || arqCestos.length()==0) {
@@ -293,13 +365,18 @@ public class HashExtensivel {
             arqDiretório.write(bd);
             
             // Cria um cesto vazio, já apontado pelo único elemento do diretório
-            Cesto c = new Cesto(quantidadeDadosPorCesto);
+            Cesto c = new Cesto(qtdCesto);
             bd = c.toByteArray();
             arqCestos.seek(0);
             arqCestos.write(bd);
         }
     }
 
+    /**
+     * É responsável por criar um novo registro na tabela hash para a instância Netflix 
+     * que é obtida por meio do método preCreate() da instância do objeto Crud.
+     * @throws Exception
+     */
     public void createHash() throws Exception {
         Netflix net = c.preCreate();
         long pos = c.createPos(net);
@@ -310,6 +387,11 @@ public class HashExtensivel {
         
     }
 
+    /**
+     * É responsável por criar um novo registro na tabela hash para a instância Netflix fornecida.
+     * @param net objetos com os dados a serem inseridos 
+     * @throws Exception
+     */
     public void createHash(Netflix net) throws Exception {
         
         long pos = c.createPos(net);
@@ -320,6 +402,16 @@ public class HashExtensivel {
         
     }
     
+    /**
+    *Cria uma entrada no hash table com a chave e o dado fornecidos, de acordo com o algoritmo de hashing
+    *e tratamento de colisões por encadeamento utilizado. A função lê o diretório, identifica a hash apropriada,
+    *recupera o cesto correspondente e realiza as operações necessárias para criar a entrada.
+    *Se a chave já existe no hash table, a função lança uma exceção.
+    *@param chave A chave da entrada a ser criada
+    *@param dado O dado associado à chave na entrada a ser criada
+    *@return true se a entrada foi criada com sucesso, false caso contrário (o cesto correspondente está cheio e não pôde ser duplicado)
+    *@throws Exception se a chave já existe no hash table
+    */
     public boolean create(int chave, long dado) throws Exception {
         
         //Carrega o diretório
@@ -334,7 +426,7 @@ public class HashExtensivel {
         
         // Recupera o cesto
         long endereçoCesto = diretório.endereço(i);
-        Cesto c = new Cesto(quantidadeDadosPorCesto);
+        Cesto c = new Cesto(qtdCesto);
         byte[] ba = new byte[c.size()];
         arqCestos.seek(endereçoCesto);
         arqCestos.read(ba);
@@ -361,11 +453,11 @@ public class HashExtensivel {
         byte pg = diretório.profundidadeGlobal;
 
         // Cria os novos cestos, com os seus dados no arquivo de cestos
-        Cesto c1 = new Cesto(quantidadeDadosPorCesto, pl+1);
+        Cesto c1 = new Cesto(qtdCesto, pl+1);
         arqCestos.seek(endereçoCesto);
         arqCestos.write(c1.toByteArray());
 
-        Cesto c2 = new Cesto(quantidadeDadosPorCesto, pl+1);
+        Cesto c2 = new Cesto(qtdCesto, pl+1);
         long novoEndereço = arqCestos.length();
         arqCestos.seek(novoEndereço);
         arqCestos.write(c2.toByteArray());
@@ -395,6 +487,12 @@ public class HashExtensivel {
 
     }
     
+    /**
+     * Lê o dado associado a uma determinada chave.
+     * @param chave a chave para a qual o dado deve ser lido.
+     * @return o dado associado à chave ou -1 se a chave não existir no arquivo.
+     * @throws Exception se ocorrer um erro ao ler o arquivo.
+    */
     public long read(int chave) throws Exception {
         
         //Carrega o diretório
@@ -409,7 +507,7 @@ public class HashExtensivel {
         
         // Recupera o cesto
         long endereçoCesto = diretório.endereço(i);
-        Cesto c = new Cesto(quantidadeDadosPorCesto);
+        Cesto c = new Cesto(qtdCesto);
         byte[] ba = new byte[c.size()];
         arqCestos.seek(endereçoCesto);
         arqCestos.read(ba);
@@ -417,36 +515,45 @@ public class HashExtensivel {
         
         return c.read(chave);
     }
-    
+
+    /**
+    Lê o valor da chave especificada na tabela hash e recupera 
+    o registro correspondente no arquivo de dados.
+    Se o registro existe, imprime as informações na tela.
+    Caso contrário, imprime uma mensagem de erro.
+    @param chave a chave a ser buscada na tabela hash
+    @throws Exception se ocorrer um erro durante a leitura do arquivo
+    */
     public void readHash(int chave) throws Exception
     {
     
         long pos = read(chave);
 
         if (pos == -1) {
-            System.out.println("Nao Tem");
+            System.out.println("Chave não encontrada!");
         } else {
             RandomAccessFile arq = new RandomAccessFile("teste.db", "rw");
             arq.seek(pos);
             char c = arq.readChar();
-            if (c == '*') {
-                System.out.println("Registro nao exite");
-            }
-            else{
-                int length = arq.readInt();
-                byte[] ba = new byte[length];
-                arq.read(ba);
-                Netflix net_temp = new Netflix();
-                net_temp.fromByteArray(ba);
-                net_temp.printar();
-            }
-          
-
+    
+            int length = arq.readInt();
+            byte[] ba = new byte[length];
+            arq.read(ba);
+            Netflix net_temp = new Netflix();
+            net_temp.fromByteArray(ba);
+            net_temp.printar();
             arq.close();
         }
 
     }
 
+    /**
+    *Atualiza o valor armazenado na tabela hash para a chave especificada com o novo dado fornecido.
+    *@param chave a chave para a qual o valor será atualizado
+    *@param novoDado o novo valor a ser armazenado na tabela hash para a chave especificada
+    *@return true se a atualização foi bem-sucedida, false caso contrário
+    *@throws Exception se ocorrer um erro durante o processo de atualização
+    */
     public boolean update(int chave, long novoDado) throws Exception {
         
         //Carrega o diretório
@@ -461,7 +568,7 @@ public class HashExtensivel {
         
         // Recupera o cesto
         long endereçoCesto = diretório.endereço(i);
-        Cesto c = new Cesto(quantidadeDadosPorCesto);
+        Cesto c = new Cesto(qtdCesto);
         byte[] ba = new byte[c.size()];
         arqCestos.seek(endereçoCesto);
         arqCestos.read(ba);
@@ -535,6 +642,13 @@ public class HashExtensivel {
 
     }
     
+    /**
+     * Esta função recebe uma chave como parâmetro e busca essa 
+     * chave em uma tabela hash para removê-la, caso ela exista.
+     * @param chave inteiro que representa a chave a ser buscada e removida na tabela hash
+     * @return true se a chave foi removida com sucesso, false caso contrário.
+     * @throws Exception lançada caso ocorra algum erro na leitura ou escrita do arquivo de diretório ou cestos.
+     */
     public boolean delete(int chave) throws Exception {
         
         //Carrega o diretório
@@ -549,7 +663,7 @@ public class HashExtensivel {
         
         // Recupera o cesto
         long endereçoCesto = diretório.endereço(i);
-        Cesto c = new Cesto(quantidadeDadosPorCesto);
+        Cesto c = new Cesto(qtdCesto);
         byte[] ba = new byte[c.size()];
         arqCestos.seek(endereçoCesto);
         arqCestos.read(ba);
@@ -565,20 +679,27 @@ public class HashExtensivel {
         return true;
     }
     
+    /**
+    *Imprime o conteúdo do diretório e dos cestos no console.
+    *@throws Exception se ocorrer algum erro durante a leitura do arquivo ou na conversão de bytes para objetos.
+    */
     public void print() {
         try {
             byte[] bd = new byte[(int)arqDiretório.length()];
             arqDiretório.seek(0);
             arqDiretório.read(bd);
             diretório = new Diretório();
-            diretório.fromByteArray(bd);   
-            System.out.println("\nDIRETÓRIO ------------------");
+            diretório.fromByteArray(bd);  
+
+            System.out.println(" ");
+            System.out.println("------- DIRETÓRI)-------");
             System.out.println(diretório);
 
-            System.out.println("\nCESTOS ---------------------");
+            System.out.println(" ");
+            System.out.println("-------CESTOS ------- ");
             arqCestos.seek(0);
             while(arqCestos.getFilePointer() != arqCestos.length()) {
-                Cesto c = new Cesto(quantidadeDadosPorCesto);
+                Cesto c = new Cesto(qtdCesto);
                 byte[] ba = new byte[c.size()];
                 arqCestos.read(ba);
                 c.fromByteArray(ba);
@@ -589,6 +710,14 @@ public class HashExtensivel {
         }
     }
 
+
+    /**
+    *Lê em arquivo CSV contendo informações de registros do tipo Netflix e cria um hash
+    *com essas informações. O hash é armazenado em um arquivo binário especificado em s2.
+    *@param s1 o caminho do arquivo CSV a ser lido
+    *@param s2 o caminho do arquivo binário em que o hash será armazenado
+    *@throws Exception se ocorrer um erro de leitura ou gravação de arquivos
+    */
     public void LerRegistroHash( String s1, String s2) throws Exception
     {
 
